aecea311bac6dde6e0717ddadfa204ee62e71418
Sajid <yeamimhossainsajid@gmail.com>
2025-08-13 15:20:27 +0600

    Web socket connection and chatting done

---
+++ src/main/java/com/example/Matches/config/chat/chatting/ChatMessageService.java
@@ -0,0 +1,33 @@
+package com.example.Matches.config.chat.chatting;
+import com.example.Matches.config.chat.chatroom.ChatRoomService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ChatMessageService {
+
+    private  ChatMessageRepo chatMessageRepo;
+    private ChatRoomService chatRoomService;
+
+    public ChatMessageService(ChatMessageRepo chatMessageRepo, ChatRoomService chatRoomService) {
+        this.chatMessageRepo = chatMessageRepo;
+        this.chatRoomService = chatRoomService;
+    }
+
+    public ChatMessage save(ChatMessage chatMessage) {
+        var chatId = chatRoomService
+                .getChatRoomId(chatMessage.getSenderId(), chatMessage.getRecipientId(), true)
+                .orElseThrow();
+        chatMessage.setChatId(chatId);
+        chatMessageRepo.save(chatMessage);
+        return chatMessage;
+    }
+
+    public List<ChatMessage> findChatMessages(String senderId, String recipientId) {
+        var chatId = chatRoomService.getChatRoomId(senderId, recipientId, false);
+        return chatId.map(chatMessageRepo::findByChatId).orElse(new ArrayList<>());
+    }
+
+}
